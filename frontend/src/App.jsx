f862913781cde3ddf8c8ecdf2148262e8dae0ec2
Tejascodes21 <tejasnikam2109@gmail.com>
2025-09-29 23:36:40 +0530

    Added Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import BookPage from "./pages/BookPage";
+import CartPage from "./pages/CartPage";
+import AboutPage from "./pages/AboutPage";
+import ConatctPage from "./pages/ConatctPage";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/books" element={<BookPage />} />
+      <Route path="/contact" element={<ConatctPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 };
